2afb2231b16a06f3b9dcd8181aa9452e516a05de
Brandon Ramirez <83187701+BrandoonRH@users.noreply.github.com>
2023-07-03 13:11:52 -0600

    Add optional chaning in de Home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
       <h1 className="text-4xl font-black">{categoryCurrent.nombre}</h1>
       <p className="text-2xl my-10">Elige y personaliza tu pedido a continuación</p>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-          {products.map(product => (
+          {products?.map(product => (
               <Product
                 key={product.imagen}
                 product={product}
